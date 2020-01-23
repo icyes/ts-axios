ab584372d48a9498ae70b078b89bb5d2cd68b8ed
icecred <asjl.chen@qq.com>
2020-01-23 22:25:14 +0800

    基础功能“

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export  type Method =
+  'get'
+  | 'GET'
+  | 'delete'
+  | 'Dlete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'PUT'
+  | 'put'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+  responseType?:XMLHttpRequestResponseType
+}
+
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export  interface AxiosPromise extends  Promise<AxiosResponse>{
+
+
+
+}
